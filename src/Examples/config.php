aac408a87ba770215f7b4db87a66c3fb28ce25dc
smallLazy <1397750928@qq.com>
2022-12-26 14:51:06 +0800

    修改配置

--- src/Examples/config.php
+++ src/Examples/config.php
@@ -1,10 +1,10 @@
 <?php
 
 return [
-    "CORP_ID"                => "",
-    "CORP_SECRET"            => "",
-    "ENCODING_AES_KEY"       => "",
-    "TOKEN"                  => "",
-    "AGENT_ID"               => "",
-    "JOIN_TRAINING_GROUP_ID" => ""
+    "CORP_ID"                => "ww53a5a250d606fc37",
+    "SECRET"                 => "hEj95-XuoftjtoHlb2dtr4DPDtQgUL878ATtXWqIKk0",
+    "ENCODING_AES_KEY"       => "m31zrcqf7Hjn2b1F3lPL0AT1oHNU8lzjk723czMrcZ4",
+    "TOKEN"                  => "pZSxRlhy9nFhe6UUnoiF",
+    "AGENT_ID"               => 1000064,
+    "JOIN_TRAINING_GROUP_ID" => "et2AytCgAATP72EgE9yePL7LAUXBMbCA"
 ];
