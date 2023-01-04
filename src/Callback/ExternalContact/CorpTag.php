fa6e9126752104043677baac921a4b3a4cab13e0
smallLazy <1397750928@qq.com>
2023-01-04 13:43:04 +0800

    optimze qw external notify

--- src/Callback/ExternalContact/CorpTag.php
+++ src/Callback/ExternalContact/CorpTag.php
@@ -4,22 +4,63 @@ namespace Buqiu\EnterpriseWechat\Callback\ExternalContact;
 
 class CorpTag
 {
-    // 企业客户标签创建
-    public static function add($xmlArray)
+    // 事件的类型
+    const EVENT_TYPE = 'change_external_tag';
+    // 企业客户标签创建事件
+    const CHANGE_TYPE_ADD = 'create';
+    // 企业客户标签变更事件
+    const CHANGE_TYPE_EDIT = 'update';
+    // 企业客户标签删除事件
+    const CHANGE_TYPE_DEL = 'delete';
+    // 企业客户标签重排事件
+    const CHANGE_TYPE_SHUFFLE = 'shuffle';
+    // 标签类型: 标签
+    const TAG_TYPE = 'tag';
+    // 标签类型: 标签组
+    const TAG_GROUP_TYPE = 'tag_group';
+
+    public static array $changeTypeMap = [
+        self::CHANGE_TYPE_ADD     => 1,
+        self::CHANGE_TYPE_EDIT    => 2,
+        self::CHANGE_TYPE_DEL     => 3,
+        self::CHANGE_TYPE_SHUFFLE => 4
+    ];
+
+    public static array $tagTypeMap = [
+        self::TAG_TYPE       => 1,
+        self::TAG_GROUP_TYPE => 2
+    ];
+
+    // 变更企业客户事件
+    public static function change($xmlArray)
+    {
+        if (!self::preHandel($xmlArray)) {
+            return false;
+        }
+
+        return [
+            'changeType' => self::$changeTypeMap['ChangeType'] ?? 0,
+            'tagType'    => self::$tagTypeMap[$xmlArray['TagType']] ?? 0,
+            'id'         => $xmlArray['Id'],
+            'strategyId' => $xmlArray['StrategyId']
+        ];
+    }
+
+    // 预处理数据
+    private static function preHandel($xmlArray): bool
     {
         if (empty($xmlArray['Event']) || empty($xmlArray['TagType']) || empty($xmlArray['ChangeType'])) {
             return false;
         }
 
-        // 创建标签时，此项为 tag，创建标签组时，此项为 tag_group
-        if ($xmlArray['TagType'] != 'tag') {
+        if ($xmlArray['Event'] != self::EVENT_TYPE) {
             return false;
         }
 
-        if ($xmlArray['ChangeType'] != 'create') {
+        if (!in_array($xmlArray['ChangeType'], self::$changeTypeMap)) {
             return false;
         }
 
-        return $xmlArray['Id'];
+        return true;
     }
 }
--- src/Callback/ExternalContact/ExternalContact.php
+++ src/Callback/ExternalContact/ExternalContact.php
@@ -4,17 +4,63 @@ namespace Buqiu\EnterpriseWechat\Callback\ExternalContact;
 
 class ExternalContact
 {
+    // 事件的类型
+    const EVENT_TYPE = 'change_external_contact';
     // 添加企业客户事件
-    public static function add($xmlArray)
+    const CHANGE_TYPE_ADD = 'add_external_contact';
+    // 编辑企业客户事件
+    const CHANGE_TYPE_EDIT = 'edit_external_contact';
+    // 删除企业客户事件
+    const CHANGE_TYPE_DEL = 'del_external_contact';
+    // 外部联系人免验证添加成员事件
+    const CHANGE_TYPE_ADD_HALF = 'add_half_external_contact';
+    // 删除跟进成员事件
+    const CHANGE_TYPE_DEL_FOLLOW_USER = 'del_follow_user';
+    // 客户接替失败事件
+    const CHANGE_TYPE_TRANSFER_FAIL = 'transfer_fail';
+
+    public static array $changeTypeMap = [
+        self::CHANGE_TYPE_ADD             => 1,
+        self::CHANGE_TYPE_EDIT            => 2,
+        self::CHANGE_TYPE_DEL             => 3,
+        self::CHANGE_TYPE_ADD_HALF        => 4,
+        self::CHANGE_TYPE_DEL_FOLLOW_USER => 5,
+        self::CHANGE_TYPE_TRANSFER_FAIL   => 6
+    ];
+
+    // 变更企业客户事件
+    public static function change($xmlArray)
+    {
+        if (!self::preHandel($xmlArray)) {
+            return false;
+        }
+
+        return [
+            'changeType'     => self::$changeTypeMap[$xmlArray['ChangeType']] ?? 0,
+            'userId'         => $xmlArray['UserID'],
+            'externalUserId' => $xmlArray['ExternalUserID'],
+            'state'          => $xmlArray['State'] ?? '',
+            'welcomeCode'    => $xmlArray['WelcomeCode'] ?? '',
+            'source'         => $xmlArray['Source'] ?? '',
+            'failReason'     => $xmlArray['FailReason'] ?? ''
+        ];
+    }
+
+    // 预处理数据
+    private static function preHandel($xmlArray): bool
     {
         if (empty($xmlArray['Event']) || empty($xmlArray['ChangeType'])) {
             return false;
         }
 
-        if ($xmlArray['ChangeType'] != 'add_external_contact') {
+        if ($xmlArray['Event'] != self::EVENT_TYPE) {
             return false;
         }
 
-        return $xmlArray['ExternalUserID'];
+        if (!in_array($xmlArray['ChangeType'], self::$changeTypeMap)) {
+            return false;
+        }
+
+        return true;
     }
 }
