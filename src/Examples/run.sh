53ca563f86d368c93b1957a5054f90f87e83db67
eva <suhong@helixlife.cn>
2024-11-06 16:57:27 +0800

    重置版本

--- src/Examples/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-php AddressBookTest.php
-
-php CorpTagTest.php
-
-php ExternalContactTest.php
-
-php MessagePushTest.php
--- src/Utils/CommonHelper.php
+++
@@ -1,6 +0,0 @@
-<?php
-namespace Buqiu\EnterpriseWechat\Utils;
-
-class CommonHelper
-{
-}
